6c94ff867ebc10feb56c5fa6f92f11fd96baa435
alumno <alumno@30010577F138B8>
2024-05-08 09:45:59 +0200

    test

---
+++ EDtest/src/main/java/com/mycompany/edtest/EDtest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.edtest;
+
+/**
+ *
+ * @author alumno
+ */
+public class EDtest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
